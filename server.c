828c83c0892009150d42056b3588a271e269a473
hefneda <hefenda@hotmail.com>
2017-09-24 22:23:24 -0500

    Add files via upload

--- server.c
+++ server.c
@@ -13,7 +13,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void ddd(void *i);
+void ddd();
 
 #define NUM_C 3
 
@@ -53,13 +53,13 @@ int main(int argc, char** argv)
     pthread_t thread[NUM_C *3];
 
     for(i = 0; i < NUM_C*2; i++)
-        pthread_create(&thread[i],NULL,ddd,NULL);
+        pthread_create(&thread[i],NULL,(void *)ddd,NULL);
 
 
     close(socket_fd);  
 }  
 
-void *ddd(void *i)
+void ddd(void)
 {
     printf("ddd");
 }
