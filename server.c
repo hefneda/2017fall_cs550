5f0f57b961b4f3c4e6154a5840a826cbe6ef0a6c
hefneda <hefenda@hotmail.com>
2017-09-25 00:04:38 -0500

    Add files via upload

--- server.c
+++ server.c
@@ -13,7 +13,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void *ddd(void *i);
+void ddd(void);
 
 #define NUM_C 3
 
@@ -27,6 +27,8 @@ int main(int argc, char** argv)
    //setup server by using sockets
     int i;
     int flag=666;
+    pthread_t thread[NUM_C *3];
+    
     if( (socket_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1 ){                            //initial Socket  
         printf("create socket error: %s(errno: %d)\n",strerror(errno),errno);  
         exit(0);  
@@ -51,17 +53,17 @@ int main(int argc, char** argv)
     printf("======waiting for client's request======\n");  
     
     //create threads
-    pthread_t thread[NUM_C *3];
+
 
     for(i = 0; i < NUM_C*2; i++)
-        flag=pthread_create(&thread[i],NULL,ddd,NULL);
+        flag=pthread_create(&thread[i],NULL,(void *)ddd,NULL);
 
     printf("%d\n",flag);  
     close(socket_fd);  
     return 0;
 }  
 
-void *ddd(void *i)
+void ddd(void)
 {
 
     printf("ddd");
