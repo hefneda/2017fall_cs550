518a578bb991086a07feaadd3c20fcd2c441197f
hefneda <hefenda@hotmail.com>
2017-09-24 22:59:58 -0500

    Add files via upload

--- client.c
+++ client.c
@@ -1,64 +1,75 @@
 /* File Name: client.c */  
-  
-#include<stdio.h>  
-#include<stdlib.h>  
-#include<string.h>  
-#include<errno.h>  
-#include<sys/types.h>  
-#include<sys/socket.h>  
-#include<netinet/in.h>  
-  
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/uio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/times.h>
+#include <sys/time.h>
+#include <time.h>
 #define MAXLINE 4096  
-  
-  
+
+void c_client(void);
+
 int main(int argc, char** argv)  
 {  
-    int    sockfd, n,rec_len;  
+    int    n,rec_len;  
     char    recvline[4096], sendline[4096];  
     char    buf[MAXLINE];  
-    struct sockaddr_in    servaddr;  
-  
-  
+
+
+
     if( argc != 2){  
-    printf("usage: ./client <ipaddress>\n");  
-    exit(0);  
+        printf("usage: ./client <ipaddress>\n");  
+        exit(0);  
     }  
-  
-  
-    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){  
-    printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);  
+
+
+    close(client_fd);  
     exit(0);  
+}  
+
+void c_client()
+{
+    int    c_client_fd;
+    struct sockaddr_un    c_clientaddr;  
+
+     if( (client_fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0){  
+        printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);  
+        exit(0);  
     }  
-  
-  
-    memset(&servaddr, 0, sizeof(servaddr));  
-    servaddr.sin_family = AF_INET;  
-    servaddr.sin_port = htons(8000);  
-    if( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){  
-    printf("inet_pton error for %s\n",argv[1]);  
-    exit(0);  
+
+
+    memset(&c_clientaddr, 0, sizeof(c_clientaddr));  
+    c_clientaddr.sin_family = AF_UNIX;  
+    strcpy(c_clientaddr.sun_path, "c_client");
+
+    if( connect(client_fd, (struct sockaddr*)&cc_clientaddr, sizeof(c_clientaddr)) < 0){  
+        printf("connect error: %s(errno: %d)\n",strerror(errno),errno);  
+        exit(0);  
     }  
-  
-  
-    if( connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){  
-    printf("connect error: %s(errno: %d)\n",strerror(errno),errno);  
-    exit(0);  
-    }  
-  
-  
+
+
     printf("send msg to server: \n");  
     fgets(sendline, 4096, stdin);  
-    if( send(sockfd, sendline, strlen(sendline), 0) < 0)  
+    if( send(client_fd, sendline, strlen(sendline), 0) < 0)  
     {  
-    printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);  
-    exit(0);  
+        printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);  
+        exit(0);  
     }  
-    if((rec_len = recv(sockfd, buf, MAXLINE,0)) == -1) {  
-       perror("recv error");  
-       exit(1);  
+    if((rec_len = recv(client_fd, buf, MAXLINE,0)) == -1) {  
+        perror("recv error");  
+        exit(1);  
     }  
     buf[rec_len]  = '\0';  
     printf("Received : %s ",buf);  
-    close(sockfd);  
-    exit(0);  
-}  
+}
