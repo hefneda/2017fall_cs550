7e8202f00867d0adb4627870146e9b9ee1123cd9
hefneda <hefenda@hotmail.com>
2017-09-24 21:18:35 -0500

    Add files via upload

--- server.c
+++ server.c
@@ -2,14 +2,19 @@
 #include<stdio.h>  
 #include<stdlib.h>  
 #include<string.h>  
-#include<errno.h>  
-#include<sys/types.h>  
-#include<sys/socket.h>  
-#include<netinet/in.h>  
-#include<pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
 #define DEFAULT_PORT 8000  
 #define MAXLINE 4096  
-
+#define NUM_C 3;  
 int setup_server();
 
 int socket_fd;
